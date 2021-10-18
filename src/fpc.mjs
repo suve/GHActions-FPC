6b31c3c0bfd6d4dc8b12d10c58b964a4ea7e5a58
suve <veg@svgames.pl>
2021-10-18 14:00:33 +0200

    Ensure latest FPC version is selected on Windows

--- src/fpc.mjs
+++ src/fpc.mjs
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as process from 'process';
 
 import * as exec from "@actions/exec";
-import { default as semverGt } from "semver/functions/gt.js";
+import { default as semverCompare } from "semver/functions/compare.js";
 import { default as semverValid } from "semver/functions/valid.js";
 
 function dirExists(path) {
@@ -14,15 +14,15 @@ function dirExists(path) {
 	}
 }
 
-function getFilteredSubdirs(dir, pattern) {
-	let filter = function(entry) {
+function getFilteredSubdirs(dir, filter) {
+	const jointFilter = function(entry) {
 		if(!entry.isDirectory()) return false;
-		return pattern.test(entry.name);
+		return filter(entry.name);
 	};
 
 	return fs
 		.readdirSync(dir, { 'withFileTypes': true })
-		.filter(filter)
+		.filter(jointFilter)
 		.map(entry => entry.name)
 	;
 }
@@ -33,9 +33,10 @@ function findFpc() {
 		return 'fpc';
 	}
 
-	const semverRegexp = /^\d+\.\d+\.\d+$/;
-
-	let dirsToCheck = [
+	const isValidSemver = function(name) {
+		return semverValid(name) !== null;
+	};
+	const dirsToCheck = [
 		'C:/fpc',
 		'C:/Program Files/fpc',
 		'C:/Program Files (x86)/fpc',
@@ -43,25 +44,46 @@ function findFpc() {
 		'C:/Program Files/lazarus/fpc',
 		'C:/Program Files (x86)/lazarus/fpc',
 	];
-	for(let d = 0; d < dirsToCheck.length; ++d) {
-		if(!dirExists(dirsToCheck[d])) continue;
 
-		let subdirs = getFilteredSubdirs(dirsToCheck[d], semverRegexp).sort();
-		for(let s = (subdirs.length - 1); s >= 0; --s) {
-			let compilerNames = [
+	let matches = [];
+	for(let dir of dirsToCheck) {
+		if(!dirExists(dir)) continue;
+
+		let subdirs = getFilteredSubdirs(dir, isValidSemver);
+		for(let subdir of subdirs) {
+			// TODO: Ignore the x86_64/win64 compilers when running on 32-bit Windows.
+			const compilerNames = [
 				'x86_64-win64',
 				'x86_64-win32',
 				'i386-win32',
 			];
-			for(let c = 0; c < compilerNames.length; ++c) {
-				let fullPath = dirsToCheck[d] + '/' + subdirs[s] + '/bin/' + compilerNames[c] + '/fpc.exe';
+			for(let cname of compilerNames) {
+				let fullPath = dir + '/' + subdir + '/bin/' + cname + '/fpc.exe';
 				if(fs.existsSync(fullPath)) {
-					return fullPath;
+					matches.push({
+						"version": subdir,
+						"type": cname,
+						"path": fullPath,
+					})
 				}
 			}
 		}
 	}
-	throw new Error('Unable to locate fpc executable');
+	if(matches.length === 0) {
+		throw new Error('Unable to locate fpc executable');
+	}
+
+	matches.sort(function(a, b) {
+		const cmp = semverCompare(a, b);
+		if(cmp !== 0) return cmp;
+
+		// This comparison makes use of the fact that, lexicographically,
+		// "x86_64" comes after "i386", and "win64" comes after "win32".
+		if(a.type > b.type) return +1;
+		if(a.type < b.type) return -1;
+		return 0;
+	});
+	return matches.pop().path;
 }
 
 async function getFpcVersion(fpc) {
@@ -89,7 +111,7 @@ async function getFpcVersion(fpc) {
 
 async function checkFpcVersion(fpc, minimumVersion) {
 	let version = await getFpcVersion(fpc);
-	if(semverGt(minimumVersion, version)) {
+	if(semverCompare(minimumVersion, version) === +1) {
 		throw new Error(`Detected FPC version ${version}, but a minimum of ${minimumVersion} is required`)
 	}
 }
