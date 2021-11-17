96184335361331e961f528d4ab00c720bb95131f
suve <veg@svgames.pl>
2021-11-18 00:33:47 +0100

    Refactor parser code a bit

--- src/parser.mjs
+++ src/parser.mjs
@@ -1,16 +1,69 @@
 import * as path from 'path';
 
+function Diagnostic(matches) {
+	this.path = path.normalize(matches[1]);
+	this.line = Number(matches[2]);
+	this.column = (matches[3] !== undefined) ? Number(matches[3]) : null;
+	this.type =  matches[4].toLowerCase();
+	this.message = matches[5];
+}
 
 function Parser(excludePath) {
-	let excludeDirs = [];
-	let excludeFiles = [];
-	for(let ex of excludePath) {
-		if(ex.endsWith(path.sep)) {
-			excludeDirs.push(ex);
-		} else {
-			excludeFiles.push(ex);
+	const fileIsExcluded = (function(){
+		let excludeDirs = [];
+		let excludeFiles = [];
+		for(let ex of excludePath) {
+			if(ex.endsWith(path.sep)) {
+				excludeDirs.push(ex);
+			} else {
+				excludeFiles.push(ex);
+			}
 		}
-	}
+
+		return function(diag) {
+			for(let excluded of excludeDirs) {
+				if(diag.path.startsWith(excluded)) return true;
+			}
+			for(let excluded of excludeFiles) {
+				if(diag.path === excluded) return true;
+			}
+			return false;
+		};
+	})();
+
+	/*
+	 * When a function is called with the wrong number of parameters, the compiler emits multiple messages:
+	 *   /example/main.pas(9,41) Error: Wrong number of parameters specified for call to "SomeFunc"
+	 *   /example/impl.pas(3,10) Error: Found declaration: SomeFunc(SmallInt):SmallInt;
+	 * If these two messages are treated separately, they'll generate a rather non-useful annotation,
+	 * attached to an error-free line, possibly in a different file. Instead, try to detect the second message type
+	 * and attach the "declaration found" information to the initial "wrong number of parameters" message.
+	 */
+	const isFoundDeclarationDiagnostic = function(diag) {
+		const foundDeclarationString = "Found declaration: ";
+		if(!diag.message.startsWith(foundDeclarationString)) return false;
+
+		const wrongNumberOfParametersString = "Wrong number of parameters specified for call to ";
+		if((previous !== null) && (previous.message.startsWith(wrongNumberOfParametersString))) {
+			// The workdir for the Action is the repo root, so a path relative to cwd will be relative to repo root.
+			// If this produces a path starting with "../", then it's a file outside the repo - in which case, refer to it using the full path.
+			let repoPath = path.relative("", diag.path);
+			if(repoPath.startsWith(`..${path.sep}`)) {
+				repoPath = diag.path;
+			}
+
+			let declaration = diag.message.substring(foundDeclarationString.length);
+			previous.message += `\nFound declaration in ${repoPath}(${diag.line},${diag.column}): ${declaration}`
+		}
+		return true;
+	};
+
+	const isStoppingDiagnostic = function(diag) {
+		if(diag.type !== "fatal") return false;
+
+		const thereWereErrorsRegexp = /^There (was|were) \d+ errors? compiling module, stopping$/;
+		return thereWereErrorsRegexp.test(diag.message);
+	};
 
 	let diagnostics = {
 		"byType": {
@@ -34,76 +87,26 @@ function Parser(excludePath) {
 		const check = diagnosticRegexp.exec(text);
 		if(check === null) return false;
 
-		const filePath = path.normalize(check[1]);
-		const line = Number(check[2]);
-		const column = (check[3] !== undefined) ? Number(check[3]) : null;
-		const type = check[4].toLowerCase();
-		const message = check[5];
+		const diag = new Diagnostic(check);
 
-		/*
-		 * When a function is called with the wrong number of parameters, the compiler emits multiple messages:
-		 *   /example/main.pas(9,41) Error: Wrong number of parameters specified for call to "SomeFunc"
-		 *   /example/impl.pas(3,10) Error: Found declaration: SomeFunc(SmallInt):SmallInt;
-		 * If these two messages are treated separately, they'll generate a rather non-useful annotation,
-		 * attached to an error-free line, possibly in a different file. Instead, try to detect the second message type
-		 * and attach the "declaration found" information to the initial "wrong number of parameters" message.
-		 */
-		const foundDeclarationString = "Found declaration: ";
-		if(message.startsWith(foundDeclarationString)) {
-			if(previous !== null) {
-				// The workdir for the Action is the repo root, so a path relative to cwd will be relative to repo root.
-				// If this produces a path starting with "../", then it's a file outside the repo - in which case, refer to it using the full path.
-				let repoPath = path.relative("", filePath);
-				if(repoPath.startsWith(`..${path.sep}`)) {
-					repoPath = filePath;
-				}
-
-				let declaration = message.substring(foundDeclarationString.length);
-				previous.message += `\nFound declaration in ${repoPath}(${line},${column}): ${declaration}`
-			}
-			return false;
-		}
-
-		// Ignore any messages pertaining to excluded files
-		for(let excluded of excludeDirs) {
-			if(filePath.startsWith(excluded)) {
-				return false;
-			}
-		}
-		for(let excluded of excludeFiles) {
-			if(filePath === excluded) {
-				return false;
-			}
-		}
-
+		// "Found declaration" messages should not generate their own annotations
+		if(isFoundDeclarationDiagnostic(diag)) return false;
 		// Ignore the "fatal error because errors occurred" message
-		if(type === "fatal") {
-			const thereWereErrorsRegexp = /^There (was|were) \d+ errors? compiling module, stopping$/;
-			if(thereWereErrorsRegexp.test(message)) {
-				return false;
-			}
-		}
+		if(isStoppingDiagnostic(diag)) return false;
+		// Ignore any messages pertaining to excluded files
+		if(fileIsExcluded(diag)) return false;
 
-		const diagObj = {
-			"path": filePath,
-			"line": line,
-			"column": column,
-			"type": type,
-			"message": message,
-		}
-		if((type === "error") || (type === "fatal")) {
-			diagnostics.byType.error.push(diagObj);
+		// "Fatal error" messages go in the same bucket as regular errors
+		const type = (diag.type === "fatal") ? "error" : diag.type;
+		diagnostics.byType[type].push(diag);
+
+		if(diagnostics.byFile.hasOwnProperty(diag.path)) {
+			diagnostics.byFile[diag.path].push(diag);
 		} else {
-			diagnostics.byType[type].push(diagObj);
+			diagnostics.byFile[diag.path] = [diag];
 		}
 
-		if(diagnostics.byFile.hasOwnProperty(filePath)) {
-			diagnostics.byFile[filePath].push(diagObj);
-		} else {
-			diagnostics.byFile[filePath] = [diagObj];
-		}
-
-		previous = diagObj;
+		previous = diag;
 		return true;
 	};
 }
